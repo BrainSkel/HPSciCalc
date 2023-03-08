5e79f7c6f72e92989612b6d4aefa4bbe2a3ff570
BrainSkel <hanne53rik@gmail.com>
2023-03-08 14:24:07 +0200

    add fonts, add windows size

--- SciCalcHP/MauiProgram.cs
+++ SciCalcHP/MauiProgram.cs
@@ -9,8 +9,8 @@ public static class MauiProgram
 			.UseMauiApp<App>()
 			.ConfigureFonts(fonts =>
 			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+				fonts.AddFont("Cairo-ExtraLight.ttf", "OpenSansRegular");
+				fonts.AddFont("Cairo-Light.ttf", "OpenSansSemibold");
 			});
 
 		return builder.Build();
